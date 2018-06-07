d2530994657963c1996ccea49864c21771ce98fc
Bill White <billdwhite@gmail.com>
2018-06-07 06:57:18 -0500

    lessons dispatching actions wip 1

---
+++ src/store/store.ts
@@ -0,0 +1,16 @@
+export class Store {
+
+    private subscribers: Function[];
+    private reducers: { [key: string]: Function };
+    private state: { [key: string]: any };
+
+
+    constructor(reducers={}, initialState={}) {
+        this.state = initialState;
+    }
+
+
+    get value() {
+        return this.state;
+    }
+}
